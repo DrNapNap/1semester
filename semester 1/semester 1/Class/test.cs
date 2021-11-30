3299297c6043163e05845a2b55fc2b45544487be
Mads-hash <56913739+Mads-hash@users.noreply.github.com>
2021-11-30 11:53:15 +0100

    a

---
+++ semester 1/semester 1/Class/test.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace semester_1
+{
+    class Test : Game1
+    {
+
+        /*protected int health;
+          public int Health { get => health; set => health = 100; }
+
+
+        public void test()
+        {
+            health = 100;
+        }
+        */
+
+        public static class test
+        {
+            public static int health { get; set; }
+    
+        }
+    }
+}
+   
+
+
+
--- semester 1/semester 1/Game1.cs
+++ semester 1/semester 1/Game1.cs
@@ -1,24 +1,39 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace semester_1
 {
     public class Game1 : Game
     {
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private SpriteFont text;
+        int health;
+
+      public void SomeMethod()
+            {
+                MyGlobals.health = 100;
+            }
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+           
         }
 
+      
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+
 
             base.Initialize();
         }
@@ -26,6 +41,8 @@ namespace semester_1
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            text = Content.Load<SpriteFont>("text");
+          
 
             // TODO: use this.Content to load your game content here
         }
@@ -44,9 +61,14 @@ namespace semester_1
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            _spriteBatch.Begin();
             // TODO: Add your drawing code here
-
+            _spriteBatch.DrawString(text, "heatlh" + health.ToString(), new Vector2(20, 20), Color.Red, 0f, Vector2.Zero, 1.5f, SpriteEffects.None, 1f);
+            
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
+  
+
     }
 }
